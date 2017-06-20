56ebcdd93d85172b764adb70aa453290d17c7b49
rajatarun <tarun17170@gmail.com>
2017-06-20 00:04:03 -0400

    converter css change

--- src/App/App.js
+++ src/App/App.js
@@ -1,21 +1,13 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
 import './App.css';
-import { Col, Row, Grid } from 'react-bootstrap'
+
 import Converter from '../Widgets/Converter/converter'
 
 class App extends Component {
   render() {
     return (
-        <Grid>
-          <Row>
-            <Col sm={3}>
-              <div className="App">
-                <Converter/>
-              </div>
-            </Col>
-          </Row>
-        </Grid>
+        <Converter/>
     );
   }
 }
--- src/Widgets/Converter/converter.js
+++ src/Widgets/Converter/converter.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import './converter.css';
 import InputTextBox    from '../../Elements/InputTextBox.js'
-
+import { Col, Row, Grid } from 'react-bootstrap'
 
 class Converter extends Component {
     render() {
         return (
-            <InputTextBox />
+            <Grid>
+                <Row>
+                    <Col sm={5} md={5} className="converter-input">
+                        <InputTextBox />
+                    </Col>
+                    <Col sm={2} md={2}>
+                        <div className="H3">=</div>
+                    </Col>
+                    <Col sm={5} md={5} className="converter-input">
+                        <InputTextBox />
+                    </Col>
+                </Row>
+            </Grid>
         );
     }
 }
