2f57716cbb7ec5d1102a79ce229ce2108ff5ca59
rajatarun <tarun17170@gmail.com>
2017-06-19 23:44:58 -0400

    bootstrap integration

--- src/App/App.js
+++ src/App/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
 import './App.css';
+import { Col, Row, Grid } from 'react-bootstrap'
 import Converter from '../Widgets/Converter/converter'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Converter/>
-      </div>
+        <Grid>
+          <Row>
+            <Col sm={3}>
+              <div className="App">
+                <Converter/>
+              </div>
+            </Col>
+          </Row>
+        </Grid>
     );
   }
 }
